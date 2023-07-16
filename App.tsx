4852275bce11a4bd35d5460307cf1f171983a9b0
NIZEYIMANA Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-07-16 09:19:46 +0200

    Bottom Tabs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Tabs = createMaterialBottomTabNavigator();
+function HomeScreen(){
+  return (
+    <></>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator 
+      activeColor="green"
+      screenOptions={{
+        tabBarLabel:false,
+        
+      }}>
+        <Tabs.Screen name="Home" component={HomeScreen} options={{
+          tabBarIcon:({color})=>(<Fontisto name="home" size={32} color={color} />)
+        }}/>
+        <Tabs.Screen name="Search" component={HomeScreen} options={{
+          tabBarIcon:({color})=>(<Ionicons name="ios-search" size={32} color={color} />)
+        }}/>
+        <Tabs.Screen name="Igtv" component={HomeScreen} options={{
+          tabBarIcon:({color})=>(<AntDesign name="plussquareo" size={32} color={color} />)
+        }}/>
+        <Tabs.Screen name="Likes" component={HomeScreen} options={{
+          tabBarIcon:({color})=>(<Ionicons name="ios-heart-outline" size={32} color={color} />)
+        }}/>
+        <Tabs.Screen name="Profile" component={HomeScreen} options={{
+          tabBarIcon:({color})=>(<Ionicons name="ios-person" size={32} color={color} />)
+        }}/>
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
