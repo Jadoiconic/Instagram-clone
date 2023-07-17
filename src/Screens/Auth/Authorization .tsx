40427f0822643e8797efb832fc817ffb7b664a47
NIZEYIMANA Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-07-17 10:10:01 +0200

    desgining login screen

--- src/Screens/Auth/Authorization .tsx
+++ src/Screens/Auth/Authorization .tsx
@@ -1,4 +1,4 @@
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, SafeAreaView, StyleSheet, Text, View, useColorScheme } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Svg, { Path } from 'react-native-svg';
@@ -9,28 +9,31 @@ const windowWidth = Dimensions.get('screen').width;
 const windowHeight = Dimensions.get('screen').height;
 
 const Authorization = ({ navigation }) => {
+
+    const theme = useColorScheme();
     return (
         <SafeAreaView>
-            <View style={styles.root}>
+            <View style={[styles.root, { backgroundColor: theme === 'light' ? 'white' : 'black', }]}>
+
                 <View style={styles.topStyle}>
-                    <Svg title="http://www.w3.org/2000/svg" width="200" height="49" viewBox="0 0 200 49" fill="none">
-                        <Path fill-rule="evenodd" clip-rule="evenodd" d={instagram} fill="#262626" />
+                <Svg title="http://www.w3.org/2000/svg" width="200" height="49" viewBox="0 0 200 49" fill="none">
+                        <Path fill-rule="evenodd" clip-rule="evenodd" d={instagram} fill={theme === 'light' ? '#262626' : 'white'} />
                     </Svg>
                     <View style={styles.account}>
                         <Image source={{ uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80" }} style={styles.image} />
                         <Text>Jacob_w</Text>
                     </View>
-                    <ButtonComp title="Log In" onPress={()=>navigation.navigate('MainApp')}/>
-                        <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
-                            <Text style={{ fontWeight: '500', marginVertical:20 }}> Switch Account</Text>
-                        </TouchableOpacity>
+                    <ButtonComp title="Log In" onPress={() => navigation.navigate('MainApp')} />
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text style={{ fontWeight: '500', marginVertical: 20, color: theme === 'light' ? 'rgba(0, 0, 0, 0.40)' : '#dede' }}> Switch Account</Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.bottomStyle}>
-                    <Text style={styles.account}>Don’t have an account?
-                        <TouchableOpacity>
-                            <Text style={{ fontWeight: 'bold' }}> Sign Up</Text>
-                        </TouchableOpacity>
+                    <Text style={[styles.account, { color: theme === 'light' ? 'rgba(0, 0, 0, 0.40)' : '#dede' }]}>Don’t have an account?
                     </Text>
+                    <TouchableOpacity>
+                        <Text style={{ fontWeight: 'bold', color: theme === 'light' ? 'rgba(0, 0, 0, 0.40)' : '#dede' }}> Sign Up</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </SafeAreaView>
@@ -43,7 +46,7 @@ const styles = StyleSheet.create({
     root: {
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor:'white',
+        backgroundColor: 'white',
     },
     topStyle: {
         height: windowHeight - 200,
@@ -59,18 +62,19 @@ const styles = StyleSheet.create({
         padding: 10,
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection:'row',
+        height:200,
     },
     account: {
         fontSize: 15,
         color: 'rgba(0, 0, 0, 0.40)',
-        marginVertical:20,
+        marginVertical: 20,
     },
     image: {
         width: 100,
         height: 100,
         borderRadius: 50,
-        paddingVertical:10,
-        marginBottom:10,
+        paddingVertical: 10,
 
     },
 })
