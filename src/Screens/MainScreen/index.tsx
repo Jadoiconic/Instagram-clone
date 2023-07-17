2e18e0efe8309abe04939e99128bce12bbdacd5f
NIZEYIMANA Jean de Dieu <86121130+Jadoiconic@users.noreply.github.com>
2023-07-17 10:22:56 +0200

    adding main screen

---
+++ src/Screens/MainScreen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Main Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default MainScreen
